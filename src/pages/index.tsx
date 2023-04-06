1a2911a4bc124516c9ca84f52cd76948079bfea0
unknown <dinhtiendong16@gmail.com>
2023-04-06 17:47:39 +0700

    tranning

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,8 @@ import React from "react";
 import { useState } from "react";
 import { HiPencil } from "react-icons/hi";
 import { AiFillDelete } from "react-icons/ai";
+import { BiPlusMedical } from "react-icons/bi";
+
 
 // alt shift f format code
 
@@ -18,13 +20,15 @@ interface TodoProp {
 }
 
 export default function Home() {
-  const [edit, setEdit] = useState(false);
+  const [edit, setEdit] = useState(0);
   const [works, setWorks] = useState("");
+  const [prevValue, setPrevValue] = useState("")
   const [todos, setTodos] = useState<TodoProp[]>([]);
 
   const handleAddState = () => {
     if (works)
-      setTodos((prev) => [
+      setTodos((prev) => 
+      [
         ...prev,
         {
           id: Math.floor(Math.random() * 1000),
@@ -42,79 +46,72 @@ export default function Home() {
   };
 
   const handleEditJob = (todoId: number) => {
-    setEdit(true);
+    setEdit(todoId);
+  };
 
-    const result = todos.find((e) => e.id === todoId);
-    if (!result) {
-      return;
-    } else {
-      if (result.id === todoId) {
-        console.log("true");
 
-        let changeValue = document.getElementById(`${result.id}`);
-        console.log(changeValue);
 
-        changeValue?.removeAttribute("disabled");
+  const handleBlur =(idItem: number)=>{
+
+    setTodos((prev)=>{
+      const curPrev = [...prev]
+      const newValue = curPrev.findIndex((e)=> e.id  === idItem)
+      curPrev[newValue].name = prevValue
+      // console.log(curPrev[newValue].name);
+      
+
+       return curPrev
+    })
+    
+  }
+
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>,idItem: number) => {
+    if (event.key === 'Enter') {
+      const result = todos.find((e) => e.id === idItem);
+      if (!result) {
+        return;
+      } else {
+        if (result.id === idItem) 
+          setEdit(0)
+          
       }
     }
-  };
-
-  const handleUpdate = (todoId: number) => {
-    // setEdit(true);
-
-    const result = todos.find((e) => e.id === todoId);
-    if (!result) {
-      return;
-    } else {
-      if (result.id === todoId) console.log(result);
-      let changeValue = document.querySelector(`input[name="${result.name}"]`);
-      console.log(changeValue);
-
-      changeValue?.setAttribute("disabled", "");
-
-      setEdit(false);
-    }
-  };
+  }
 
   return (
-    <>
-      <div className="flex flex-col gap-8 h-screen items-center border border-red-500 justify-center ">
-        <div className="flex gap-5 h-screen justify-center items-center">
+    <div className="flex items-center justify-center h-screen w-vw bg-[#489cc1]">
+      <div className="flex flex-col w-[500px] h-auto gap-8 bg-white items-center justify-center ">
+        <div className="flex w-full items-center justify-center text-5xl font-bold">TODOLIST</div>
+        <div className="flex gap-5  ">
           <input
             value={works}
             type={"text"}
             onChange={(e) => setWorks(e.target.value)}
             id="firstInput"
             className="outline-none border border-blue-500 px-4 py-2 w-[400px]"
+            placeholder="What needs to be done"
           />
 
-          <input
-            onClick={handleAddState}
-            type="button"
-            className="outline-none px-4 py-2 bg-blue-500 rounded-md text-white"
-            value="Add"
-          />
+          <button className="px-5 py-5 bg-[#489cc1] cursor-pointer text-black"  onClick={handleAddState}>
+            <BiPlusMedical size={24}/>
+          </button>
+
         </div>
 
         <ul>
           {todos.map((item, index) => {
             return (
               <li key={index} className="flex gap-10 items-center">
-                {/* setReworks(item.name) */}
-                {edit === false ? (
+               
+
                   <input
-                    id={`${item.id}`}
-                    type="text"
-                    value={item.name}
-                    disabled={true}
-                  />
-                ) : (
-                  <input
-                  disabled
+                  disabled={(edit !== item.id)}
                     id={`${item.id}`}
                     type="text"
                     name={item.name}
                     value={item.name}
+                    onBlur={() =>handleBlur(item.id)}
+                    onFocus={()=> { setPrevValue(item.name)}}
                     onChange={(e) => {
                       setTodos((prev) => {
                         const newTodos = [...prev];
@@ -125,37 +122,28 @@ export default function Home() {
                         newTodos[index].name = e.target.value;
                         return newTodos;
                       });
-                    }}
+            }}
+            onKeyDown={(e)=>{handleKeyDown(e,item.id)}}
                   />
-                )}
-                <span
+                
+                <button
                   onClick={() => handleDeleteJob(item.id)}
                   className="my-2 cursor-pointer px-5 py-5 bg-blue-300 hover:bg-red-600 "
                 >
                   <AiFillDelete />
-                </span>
-                <span
+                </button>
+                <button
                   onClick={() => handleEditJob(item.id)}
                   className="my-2 cursor-pointer px-5 py-5 bg-blue-300 hover:bg-red-600 "
                 >
                   <HiPencil />
-                </span>
-
-                <span
-                  onClick={() => handleUpdate(item.id)}
-                  className={`my-2 cursor-pointer px-5 py-5 bg-blue-300 hover:bg-red-600 ${
-                    edit === false
-                      ? "opacity-0 cursor-default disabled"
-                      : "opacity-100 cursor-pointer"
-                  }`}
-                >
-                  Save
-                </span>
+                </button>
+                
               </li>
             );
           })}
         </ul>
       </div>
-    </>
+    </div>
   );
 }
