5069aae0aa3e217135602e15237cb0dd92b5491a
unknown <dinhtiendong16@gmail.com>
2023-04-08 08:40:27 +0700

    feat: click btn edit focus input and click checkbox add line-through for input

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,38 +2,36 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { useState } from "react";
 import { HiPencil } from "react-icons/hi";
 import { AiFillDelete } from "react-icons/ai";
 import { BiPlusMedical } from "react-icons/bi";
 
 
-// alt shift f format code
-
-const inter = Inter({ subsets: ["latin"] });
-// interface
 interface TodoProp {
   id: number;
   name: string;
-  status: string;
+  status: boolean;
 }
-
 export default function Home() {
-  const [edit, setEdit] = useState(0);
+
+  const [edit, setEdit] = useState(-1);
   const [works, setWorks] = useState("");
-  const [prevValue, setPrevValue] = useState("")
+  const [prevValue, setPrevValue] = useState("");
+  const [status, setStatus] = useState(false);
   const [todos, setTodos] = useState<TodoProp[]>([]);
+  const ref = useRef<Array<HTMLDivElement | null>>([]);
+
 
   const handleAddState = () => {
     if (works)
-      setTodos((prev) => 
-      [
+      setTodos((prev) => [
         ...prev,
         {
           id: Math.floor(Math.random() * 1000),
           name: works,
-          status: "TODO",
+          status: status,
         },
       ]);
     setWorks("");
@@ -45,87 +43,140 @@ export default function Home() {
     });
   };
 
-  const handleEditJob = (todoId: number) => {
-    setEdit(todoId);
+  const handleEditJob = (index: number) => {
+
+    setEdit(index);
+    console.log(index);
+    
   };
 
+  const handleBlur = (idItem: number) => {
+    setTodos((prev) => {
+      const curPrev = [...prev];
+      const newValue = curPrev.findIndex((e) => e.id === idItem);
+      curPrev[newValue].name = prevValue;
 
+      return curPrev;
+    });
+  };
 
-  const handleBlur =(idItem: number)=>{
-
-    setTodos((prev)=>{
-      const curPrev = [...prev]
-      const newValue = curPrev.findIndex((e)=> e.id  === idItem)
-      curPrev[newValue].name = prevValue
-      // console.log(curPrev[newValue].name);
-      
-
-       return curPrev
-    })
-    
-  }
-
-  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>,idItem: number) => {
-    if (event.key === 'Enter') {
+  const handleKeyDown = (
+    event: React.KeyboardEvent<HTMLInputElement>,
+    idItem: number
+  ) => {
+    if (event.key === "Enter") {
       const result = todos.find((e) => e.id === idItem);
-      if (!result) {
-        return;
-      } else {
-        if (result.id === idItem) 
-          setEdit(0)
-          
+
+      if (result.id === idItem) {
+        setEdit(-1);
       }
     }
-  }
+  };
+
+  const handleCheck = (
+    e: React.ChangeEvent<HTMLInputElement>,
+    idItem: number  ) => {
+    console.log(e.target.checked);
+
+    setTodos((prev) => {
+      const updateCheck = [...prev];
+      const newValue = updateCheck.findIndex((e) => e.id === idItem);
+
+      if( updateCheck[newValue].id === idItem)
+      {
+        setStatus((prev) => !prev);
+        
+      }
+
+      return updateCheck;
+    });
+
+    
+
+  };
+  
+  useEffect(() =>{
+    if(ref.current[edit])
+    {
+        console.log(ref.current[edit]);
+        
+      ref.current[edit]!.focus();
+    }
+  },[edit])
+
 
   return (
     <div className="flex items-center justify-center h-screen w-vw bg-[#489cc1]">
-      <div className="flex flex-col w-[500px] h-auto gap-8 bg-white items-center justify-center ">
-        <div className="flex w-full items-center justify-center text-5xl font-bold">TODOLIST</div>
-        <div className="flex gap-5  ">
-          <input
-            value={works}
-            type={"text"}
-            onChange={(e) => setWorks(e.target.value)}
-            id="firstInput"
-            className="outline-none border border-blue-500 px-4 py-2 w-[400px]"
-            placeholder="What needs to be done"
-          />
+      <div className="flex flex-col h-auto gap-8 bg-white items-center justify-center ">
+        <div className="flex w-full items-center justify-center text-5xl font-bold">
+          TODOLIST
+        </div>
+        <div className="flex ">
+          <div className="flex items-center justify-center ml-5 border border-black mr-9">
+            <input
+              onFocus={() => {}}
+              value={works}
+              type={"text"}
+              onChange={(e) => setWorks(e.target.value)}
+              id="firstInput"
+              className="outline-none px-4 py-2 w-[300px] box-border"
+              placeholder="What needs to be done"
+            />
+            <select className="w-[100px]">
+              <option>Hight</option>
+              <option>Medium</option>
+              <option>Low</option>
+            </select>
+          </div>
 
-          <button className="px-5 py-5 bg-[#489cc1] cursor-pointer text-black"  onClick={handleAddState}>
-            <BiPlusMedical size={24}/>
+          <button
+            className="px-5 py-5 bg-[#489cc1] cursor-pointer text-black"
+            onClick={handleAddState}
+          >  
+            <BiPlusMedical size={24} />
           </button>
-
         </div>
 
         <ul>
-          {todos.map((item, index) => {
+          {todos.map((item, i) => {
             return (
-              <li key={index} className="flex gap-10 items-center">
-               
+              <li key={i} className="flex gap-10 items-center">
+                <input
+                  type="checkbox"
+                  checked={status}
+                  onChange={(e) => {
+                    handleCheck(e,item.id);
+                  }}
+                />
+                <input
+                  disabled={edit !== i}
+                  id={`${item.id}`}
+                  type="text"
+                  name={item.name}
+                  ref={(el) => (ref.current[i] = el)}
+                  value={item.name}
+                  className={`${(status === false ) ? {} : "line-through"}`}
+                  
+                  onBlur={() => handleBlur(item.id)}
+                  onFocus={() => {
+                    setPrevValue(item.name);
+                  }}
+                  onChange={(e) => {
+                    setTodos((prev) => {
+                      const newTodos = [...prev];
+                      const index = prev.findIndex(
+                        (todo) => todo.id === item.id
+                      );
 
-                  <input
-                  disabled={(edit !== item.id)}
-                    id={`${item.id}`}
-                    type="text"
-                    name={item.name}
-                    value={item.name}
-                    onBlur={() =>handleBlur(item.id)}
-                    onFocus={()=> { setPrevValue(item.name)}}
-                    onChange={(e) => {
-                      setTodos((prev) => {
-                        const newTodos = [...prev];
-                        const index = prev.findIndex(
-                          (todo) => todo.id === item.id
-                        );
+                      newTodos[index].name = e.target.value;
+                      return newTodos;
+                    });
+                  }}
+                  onKeyDown={(e) => {
+                    handleKeyDown(e, item.id);
+                  }}
+                />
 
-                        newTodos[index].name = e.target.value;
-                        return newTodos;
-                      });
-            }}
-            onKeyDown={(e)=>{handleKeyDown(e,item.id)}}
-                  />
-                
                 <button
                   onClick={() => handleDeleteJob(item.id)}
                   className="my-2 cursor-pointer px-5 py-5 bg-blue-300 hover:bg-red-600 "
@@ -133,12 +184,11 @@ export default function Home() {
                   <AiFillDelete />
                 </button>
                 <button
-                  onClick={() => handleEditJob(item.id)}
+                  onClick={() => handleEditJob(i)}
                   className="my-2 cursor-pointer px-5 py-5 bg-blue-300 hover:bg-red-600 "
                 >
                   <HiPencil />
                 </button>
-                
               </li>
             );
           })}
